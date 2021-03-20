7ccebc6a91309c4c58e61eee4f00a4a17a1b2096
Anton Felix Lorenzen <anfelor@posteo.de>
2021-03-20 20:54:18 +0100

    Initial deploy

---
+++ precache-manifest.73b73814c1fb27185aeb69e58937a6c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89cbd67404f17178532",
+    "url": "/css/app.889322bc.css"
+  },
+  {
+    "revision": "e5a0e995c423eb3f73c72bc3738e83dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0798d3488620e0d7e102",
+    "url": "/js/about.3e5bafd8.js"
+  },
+  {
+    "revision": "a89cbd67404f17178532",
+    "url": "/js/app.b8bf7545.js"
+  },
+  {
+    "revision": "053ba5258f30a881f12a",
+    "url": "/js/chunk-vendors.1002dcd4.js"
+  },
+  {
+    "revision": "021988f76de8c0953876d982229a2575",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ded443ed3d9547f852b5ba9058ff68eb",
+    "url": "/naproche.js"
+  },
+  {
+    "revision": "c8dc043b2530abc116f23031204757d0",
+    "url": "/prover.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "61f2866adc69c56ce9ccf6bb60052697",
+    "url": "/spass.wasm"
+  }
+]);
